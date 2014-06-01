e93fee54ff417d00ad5f24f228fb04d00002956c
Alessio Cimarelli <jenkin@dataninja.it>
2014-06-01 13:20:46 +0200

    Create rawtext2title.js
    First implementation in js using Chromium console.

---
+++ rawtext2title.js
@@ -0,0 +1,31 @@
+/* 
+ * In Chromium console from SVG file tab.
+ */
+
+// Text nodes 
+var texts = document.getElementsByTagName("text");
+
+for (var k in texts) {
+  // Current text node
+  var text = texts[k];
+  // If text is a node
+  if (text.getAttribute) {
+    // Text transform attribute
+    var transform = text.getAttribute("transform");
+    // Text and circle are linked by the same transform attribute!
+    var circle = document.querySelectorAll("circle[transform='"+transform+"']")[0];
+    // New title element
+    var title = document.createElement("title");
+    // Copy text content from text to title
+    title.textContent = text.textContent;
+    // Append title node to circle
+    circle.appendChild(title);
+    // Remove old text node
+    text.parentNode.removeChild(text);
+  }
+}
+
+/*
+ * Then copy as HTML the svg element (root element) and paste it in a new file .svg.
+ * That's it!
+ */
